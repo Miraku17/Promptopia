f0bdb67f5313fc84a3a6abb3417bca4fbfa7020a
Zian Rinzler Valles <83147228+Miraku17@users.noreply.github.com>
2024-06-12 17:02:50 +0800

    with authentication

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,23 +1,25 @@
 import "@styles/globals.css";
-import Nav from "@components/Nav"
-import Provider from "@components/Provider"
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
 
 export const metadata = {
   title: "Promptopia",
   description: "Discover and share AI prompts",
 };
 
-const RootLayout = ({children}) => {
+const RootLayout = ({ children }) => {
   return (
     <html lang="en">
       <body>
-        <div className="main">
-            <div className = "gradient" />
-        </div>
-        <main className="app">
+        <Provider>
+          <div className="main">
+            <div className="gradient" />
+          </div>
+          <main className="app">
             <Nav />
             {children}
-        </main>
+          </main>
+        </Provider>
       </body>
     </html>
   );
